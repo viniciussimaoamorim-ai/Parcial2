b5b9ef8648ce070df74cbbbb6fcecf4509247318
viniciussimaoamorim-ai <vinicius.simao.amorim@escola.pr.gov.br>
2026-03-25 08:45:56 -0300

    Update Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,3 +1,20 @@
-#3 lista de objetos
-material_escolar = ["caderno, caneta, borracha, lapis, marca texto"]
-print(material_escolar) 
+# Lista de Objetos
+def criar_lista_material():
+    material_escolar = []
+    print("Digite os materiais escolares. Quando terminar, digite 'fim'.")
+
+    while True:
+        item = input("Digite um material: ")
+        if item.lower() == 'fim':
+            break
+        material_escolar.append(item)
+
+    return material_escolar
+
+# Criando a lista com os materiais
+material_escolar = criar_lista_material()
+
+# Imprimindo a lista de materiais escolares
+print("\nLista de materiais escolares:")
+for item in material_escolar:
+    print(item)
